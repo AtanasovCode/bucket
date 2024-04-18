3b33b0d1eef0d7d7beb3e00facd067a35cbcca3b
AtanasovCode <38294514+AtanasovCode@users.noreply.github.com>
2024-04-18 12:06:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard from "./routes/Dashboard";
 
 const App = () => {
-
-  const firebaseConfig = {
-
-    apiKey: "AIzaSyBuhMJm0P4CM-WzWoOr1KD3E1kq66eOkc8",
-    authDomain: "bucket-cd9fc.firebaseapp.com",
-    projectId: "bucket-cd9fc",
-    storageBucket: "bucket-cd9fc.appspot.com",
-    messagingSenderId: "77799167543",
-    appId: "1:77799167543:web:4ad4efb9fcef4f851291b2",
-    measurementId: "G-5F3SCRQR21"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  const analytics = getAnalytics(app);
-
-
   const router = createBrowserRouter([
     {
       path: "/",
