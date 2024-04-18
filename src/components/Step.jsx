1ae5b2644852c1b1b091b1f16e39589138591d6d
AtanasovCode <a_ljupce@yahoo.com>
2024-04-18 19:38:30 +0200

    made steps fully responsive and functional

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -5,12 +5,12 @@ const Step = ({
 }) => {
     return (
         <div
-            className={`w-8 h-8 flex items-center justify-center rounded-full cursor-pointer
+            className={`w-8 h-8 min-w-8 flex items-center justify-center rounded-full cursor-pointer
             ${step === 1 || step === currentStep ? 'bg-accent' : 'bg-inactive'}
-            ${step !== currentStep && 'text-light'}
+            ${step !== 1 && step !== currentStep && 'text-light'}
             transition-colors duration-1000
         `}
-            onClick={() => handleClick()}
+            onClick={() => handleClick(step)}
         >
             {step}
         </div>
--- src/routes/Add.jsx
+++ src/routes/Add.jsx
@@ -9,15 +9,19 @@ const Add = ({
 
     const styleWidth = currentStep === 2 ? "w-full" : "w-0";
 
-    const changeStep = () => {
-        currentStep === 1 ? setCurrentStep(2) : setCurrentStep(1);
+    const changeStep = (step) => {
+        setCurrentStep(step);
     }
 
     return (
         <div className="">
-            <div className="flex items-center justify-center transition-colors duration-500">
+            <div className="
+                flex items-center justify-center transition-colors duration-500
+                absolute bottom-[8%] left-[50%] translate-x-[-50%]
+                w-[60%] sm:w-[30%] lg:w-[12%]
+            ">
                 <Step step={1} handleClick={changeStep} currentStep={currentStep} />
-                <div className={`bg-inactive w-[30vw] h-1 relative`}>
+                <div className={`bg-inactive w-full h-1 relative`}>
                     <div className={`absolute ${styleWidth} h-full top-0 left-0 transition-all duration-500 bg-accent`}></div>
                 </div>
                 <Step step={2} handleClick={changeStep} currentStep={currentStep} />
